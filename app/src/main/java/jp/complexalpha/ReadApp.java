81f566b13e2a87a92a232736a284bed7b74ba21d
Yusuke Shinohara <complex.alpha.cxa@gmail.com>
2026-04-13 17:31:18 +0900

    POI Read

--- app/src/main/java/jp/complexalpha/ReadApp.java
+++ app/src/main/java/jp/complexalpha/ReadApp.java
@@ -1,14 +1,16 @@
 package jp.complexalpha;
 
 import jp.complexalpha.fastx.XlsxFastx;
+import jp.complexalpha.poip.XlsxPoi;
 
 import java.io.IOException;
 
 public class ReadApp {
     public static void main(String[] args) throws IOException {
         System.out.println("ReadApp START");
-
-        XlsxFastx.readExcel();
+        //XlsxFastx.readExcel();
         System.out.println("ReadApp END");
+
+        XlsxPoi.readXlsx();
     }
 }
